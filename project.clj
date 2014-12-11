582ac49086c750d99ff203e0175dcf121d417cb5
Fraz <frazer.irving@mailonline.co.uk>
2014-12-11 14:00:55 +1100

    newer chromebuild version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chrome-extension/lein-template "0.1.3"
+(defproject chrome-extension/lein-template "0.1.4"
   :description "A leiningen template for generating lein-chromebuild projects"
   :url "https://github.com/clumsyjedi/lein-chrome-extension"
   :license {:name "Eclipse Public License"
