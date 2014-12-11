c69f4de9ab443a84d7f5ddd3360e76faeb512e59
Fraz <frazer.irving@mailonline.co.uk>
2014-12-11 14:23:58 +1100

    Adds scm details

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject chrome-extension/lein-template "0.1.4"
+(defproject chrome-extension/lein-template "0.1.5"
   :description "A leiningen template for generating lein-chromebuild projects"
   :url "https://github.com/clumsyjedi/lein-chrome-extension"
   :license {:name "Eclipse Public License"
             :url "https://github.com/clumsyjedi/lein-chrome-extension"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git" :url "https://github.com/clumsyjedi/lein-chrome-extension"})
